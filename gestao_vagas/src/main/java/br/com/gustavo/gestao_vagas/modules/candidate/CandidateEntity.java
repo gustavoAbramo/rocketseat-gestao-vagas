2a12393374bd12500e58c9be3cf2465014beae11
Coffee-Gutts <gustavoabramooo@gmail.com>
2026-03-29 23:56:02 -0300

    feat: add validation

--- gestao_vagas/src/main/java/br/com/gustavo/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/gustavo/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package br.com.gustavo.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +12,14 @@ public class CandidateEntity {
 
     private UUID uuid;
     private String name;
+
+    @Email(message = "O campo (email) deve conter um endereço de email válido.")
     private String email;
+
+    @Pattern(regexp = "^(?!\\s*$)[a-zA-Z0-9]+$", message = "O campo (username) deve conter apenas letras e números, sem espaços em branco.")
     private String username;
+
+    @Length(min = 5, max = 100)
     private String password;
     private String description;
     private String Curriculum;
--- gestao_vagas/src/main/java/br/com/gustavo/gestao_vagas/modules/candidate/controller/CandidateController.java
+++ gestao_vagas/src/main/java/br/com/gustavo/gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -2,6 +2,7 @@ package br.com.gustavo.gestao_vagas.modules.candidate.controller;
 
 
 import br.com.gustavo.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato criado: ");
         System.out.println(candidateEntity.getEmail());
     }
